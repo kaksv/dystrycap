1a26f3f3b58977caffe0adbf5766a684105dbe2a
kaksv <kakoozavian@gmail.com>
2025-08-30 20:27:34 +0300

    url search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
+import { sdk } from '@farcaster/miniapp-sdk'
 import Herodash from './Dash/Dash'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    async function sdkactions() {
+      await sdk.actions.ready()
+    }
+    sdkactions();
+  
+  }, [])
   
 
   return (
